e5e265ff6a7becf936d0293614a490355734c3e2
roshith <roshithprakash7@gmail.com>
2026-01-17 23:10:07 +0530

    Prod link added

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const devURL = "http://localhost:4000/api/v1";
+export const prodURL = "https://hyphen-backend-qbkv.onrender.com/api/v1";
 
 // Creating an instance of axios to make API calls to server
 export const axiosInstance = axios.create({
-  baseURL: devURL,
+  baseURL: prodURL,
 });
